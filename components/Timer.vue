db1d5879bd9ff023dfcb7ab5ab8655f1f80e21fc
hisasue <hisa@flatz.jp>
2021-03-03 21:06:03 +0900

    1-3の途中。タイマーの時間をフォーマットして表示する部分を実装した

--- components/Timer.vue
+++ components/Timer.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
     <div class="title">TimerWorld</div>
-    <input id="timeeeee" v-model="timeeeee" class="border-gray-300">
-    <div id="timer">00:0{{ timeeeee }}:00</div>
+    <input id="timeeeee" v-model="timeeeee" class="border-gray-300" />
+    <div id="timer">{{ getFormattedTime(timeeeee) }}</div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      timeeeee: 1
-    }
-  }
-})
+      timeeeee: 1,
+    };
+  },
+  methods: {
+    setTimer: function (seconds) {
+      this.timeeeee = seconds;
+    },
+    getFormattedTime: function (setSeconds) {
+      const hours = Math.floor(setSeconds / (60 * 60));
+      const hours_string = this.get0PadNumber(hours, 2);
+
+      const minutes = Math.floor((setSeconds % (60 * 60)) / 60);
+      const minutes_string = this.get0PadNumber(minutes, 2);
+
+      const seconds = setSeconds - hours * (60 * 60) - minutes * 60;
+      const seconds_string = this.get0PadNumber(seconds, 2);
+
+      return hours_string + ":" + minutes_string + ":" + seconds_string;
+    },
+    get0PadNumber: function (number, length) {
+      return number.toString().padStart(length, "0");
+    },
+  },
+});
 </script>
--- test/Timer.spec.js
+++ test/Timer.spec.js
@@ -17,7 +17,7 @@ describe('Timer', () => {
   test('1分のタイマーが表示されていること', () => {
     const wrapper = mount(Timer)
     const timer = wrapper.find('#timer')
-    expect(timer.text()).toBe('00:01:00')
+    expect(timer.text()).toBe('00:00:01')
   })
 
   test('時間を設定するテキストボックスが表示されていること', () => {
@@ -31,7 +31,28 @@ describe('Timer', () => {
     const textBox = wrapper.find('#timeeeee')
     const timer = wrapper.find('#timer')
     await textBox.setValue(3)
-    expect(timer.text()).toBe('00:03:00')
+    expect(timer.text()).toBe('00:00:03')
   })
+
+  test('3を渡したら00:00:03 というフォーマットにして文字列を返す', () => {
+    const wrapper = mount(Timer)
+    expect(wrapper.vm.getFormattedTime(3)).toBe('00:00:03')
+  })
+
+  test('タイマーを3秒にセットするとタイマーの表示が00:00:03になること', async () => {
+    const wrapper = mount(Timer)
+    const timer = wrapper.find('#timer')
+    await wrapper.vm.setTimer(3)
+    expect(timer.text()).toBe('00:00:03')
+  })
+
+  // test('3秒経ったらタイマーが0になること', () => {
+  //   const wrapper = mount(Timer)
+  //   const timer = wrapper.find('#timer')
+  //   wrapper.setTimer(3)
+  //   wrapper.startTimer()
+  //   // 3秒待つ
+  //   expect(timer.text()).toBe('00:00:00')
+  // })
 
 })
