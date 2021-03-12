2a569e9dacb87bb8ed70f6e2b6ec54c5bb343376
hisasue <hisa@flatz.jp>
2021-03-12 20:56:55 +0900

    refactor: add beforeEach to setup wrapper

--- test/Timer.spec.js
+++ test/Timer.spec.js
@@ -3,31 +3,31 @@ import Timer from '@/components/Timer.vue'
 import { expect, test } from '@jest/globals'
 
 describe('Timer', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(Timer)
+  });
+
   test('is a Vue instance', () => {
-    const wrapper = mount(Timer)
     expect(wrapper.vm).toBeTruthy()
   })
 
   test('view title', () => {
-    const wrapper = mount(Timer)
     const div = wrapper.find('#title')
     expect(div.text()).toBe('Timeboxman')
   })
 
   test('1秒のタイマーが表示されていること', () => {
-    const wrapper = mount(Timer)
     const timer = wrapper.find('#timer')
     expect(timer.text()).toBe('00:00:01')
   })
 
   test('時間を設定するテキストボックスが表示されていること', () => {
-    const wrapper = mount(Timer)
     const textBox = wrapper.find('#input-time')
     expect(textBox.exists()).toBe(true)
   })
 
   test('テキストボックスに入力した時間がタイマーに表示されていること', async () => {
-    const wrapper = mount(Timer)
     const textBox = wrapper.find('#input-time')
     const timer = wrapper.find('#timer')
     await textBox.setValue(3)
@@ -35,13 +35,11 @@ describe('Timer', () => {
   })
 
   test('3を渡したら00:00:03 というフォーマットにして文字列を返す', () => {
-    const wrapper = mount(Timer)
     wrapper.vm.timerTime = 3
     expect(wrapper.vm.getFormattedTime).toBe('00:00:03')
   })
 
   test('タイマーを3秒にセットするとタイマーの表示が00:00:03になること', async () => {
-    const wrapper = mount(Timer)
     const timer = wrapper.find('#timer')
     await wrapper.vm.setTimer(3)
     expect(timer.text()).toBe('00:00:03')
@@ -49,7 +47,6 @@ describe('Timer', () => {
 
   const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
   test('2秒経ったらタイマーが0になること', async () => {
-    const wrapper = mount(Timer)
     const timer = wrapper.find('#timer')
     wrapper.vm.setTimer(2)
     wrapper.vm.startTimer()
@@ -58,13 +55,11 @@ describe('Timer', () => {
   })
 
   test('スタートボタンが表示されていること', () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     expect(startButton.exists()).toBe(true)
   })
 
   test('スタートボタンを押すとタイマーがスタートすること', () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     wrapper.vm.setTimer(2)
     expect(wrapper.vm.$data.started).toBe(false)
@@ -73,13 +68,11 @@ describe('Timer', () => {
   })
 
   test('ストップボタンが表示されていること', () => {
-    const wrapper = mount(Timer)
     const stopButton = wrapper.find('#stop-button')
     expect(stopButton.exists()).toBe(true)
   })
 
   test('ストップボタンを押すとタイマーがストップすること', () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     const stopButton = wrapper.find('#stop-button')
     wrapper.vm.setTimer(10)
@@ -90,7 +83,6 @@ describe('Timer', () => {
   })
 
   test('タイマーがカウントダウンしている間は時間入力のテキストボックスに入力できないようになること', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     wrapper.vm.setTimer(10)
     await startButton.trigger("click")
@@ -99,25 +91,21 @@ describe('Timer', () => {
   })
 
   test('タイマーが止まっているときはSTOPボタンが赤くなる', () => {
-    const wrapper = mount(Timer)
     const stopButton = wrapper.find('#stop-button')
     expect(stopButton.attributes().class.split(' ').includes('bg-red-200')).toBe(true)
   })
 
   test('タイマーがカウントダウンしているときはSTARTボタンが緑になる', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     await startButton.trigger('click')
     expect(startButton.attributes().class.split(' ').includes('bg-green-200')).toBe(true)
   })
 
   test('画面ロード時はタイマー完了状態でないこと', () => {
-    const wrapper = mount(Timer)
     expect(wrapper.vm.$data.timerFinished).toBe(false)
   })
 
   test('カウントダウンがゼロになったらタイマー完了状態になること', async () => {
-    const wrapper = mount(Timer)
     await wrapper.vm.setTimer(1)
     await wrapper.vm.startTimer()
     await sleep(2000)
@@ -126,7 +114,6 @@ describe('Timer', () => {
   })
 
   test('タイマー完了状態は、メッセージボックスに完了メッセージが表示されていること', async () => {
-    const wrapper = mount(Timer)
     await wrapper.vm.finishTimer()
 
     const message = wrapper.find('#message-box')
@@ -134,7 +121,6 @@ describe('Timer', () => {
   })
 
   test('タイマー完了状態は、タイマーの背景色が赤になること', async () => {
-    const wrapper = mount(Timer)
     await wrapper.vm.finishTimer()
 
     const timer = wrapper.find('#timer')
@@ -142,7 +128,6 @@ describe('Timer', () => {
   })
 
   test('タイマーが完了状態でない場合は、メッセージボックスが存在しないこと', done => {
-    const wrapper = mount(Timer)
     wrapper.vm.$data.timerFinished = false
 
     wrapper.vm.$nextTick(() => {
@@ -153,7 +138,6 @@ describe('Timer', () => {
   })
 
   test('スタートボタンを押すとタイマー完了状態がリセットされること', () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     wrapper.vm.$data.timerFinished = true
 
@@ -163,7 +147,6 @@ describe('Timer', () => {
   })
 
   test('タイマー終了後、メッセージをクリックするとタイマーがリセットされること', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
 
     await wrapper.vm.setTimer(1)
@@ -176,7 +159,6 @@ describe('Timer', () => {
   })
 
   test('タイマーが止まっているときにリセットボタンがクリックされたらタイマーがリセットされること', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     const resetButton = wrapper.find('#reset-button')
     wrapper.vm.setTimer(10)
@@ -187,7 +169,6 @@ describe('Timer', () => {
   })
 
   test('ストップボタンをクリック直後にスタートボタンが1秒間クリックできないようになっていること', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     const stopButton = wrapper.find('#stop-button')
     wrapper.vm.setTimer(10)
@@ -199,13 +180,15 @@ describe('Timer', () => {
   })
 
   test('タイマーの時間（timerTime）が0のときにタイマーをスタートできないこと', async () => {
-    const wrapper = mount(Timer)
     const startButton = wrapper.find('#start-button')
     wrapper.vm.timerTime = 0
     await startButton.trigger('click')
 
     expect(wrapper.vm.started).toBe(false)
   })
+
+  // test('タイマー終了サウンドがTimerコンポーネントに存在している', () => {
+  // })
 
   const isReset = (timer) => {
     const { inputTime, timerTime, started, timerFinished, message } = timer.vm.$data
