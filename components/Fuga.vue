9d72714d683301fd0c0c8471d1e46b9f78d32a0d
hisasue <hisa@flatz.jp>
2021-04-08 20:54:44 +0900

    vue の値受け渡しを確認するためのコードを追加したが、うんこなのでうんこブランチにした2

---
+++ components/Fuga.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="border" @click="buttonClickEventHandler">
+      Fuga [{{ baa }}]
+    </button>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  props: ["baa"],
+  data() {
+    return {};
+  },
+  methods: {
+    buttonClickEventHandler: function (e) {
+      this.$emit("gee", "AAAAAA");
+    },
+  },
+});
+</script>
---
+++ components/Hoge.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Hello Hoge</p>
+    <Fuga :baa="year" @gee="fromFoo" />
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      year: 1
+    };
+  },
+  methods: {
+    fromFoo: function(e) {
+      console.log(e)
+    }
+  }
+});
+</script>
