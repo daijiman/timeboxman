0d983bc1d752b509379a82dc35cbbf27b3fd1b97
hisasue <hisa@flatz.jp>
2022-01-12 12:30:10 +0900

    update modules and fixed problems by this update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  globals: {
+    window: {}
+  },
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/fileMock.js',
     '^@/(.*)$': '<rootDir>/$1',
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
+// setImmediate が削除されたための対応 
+// fix(env-jsdom): remove setImmediate and clearImmediate #11222
+// https://github.com/facebook/jest/pull/11222
+import * as timers from "timers"
+if(typeof setImmediate !== "function") {
+	global.setImmediate = timers.setImmediate
+	global.clearImmediate = timers.clearImmediate
+}
+
 window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
 window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
